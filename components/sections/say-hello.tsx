14a7768b4c127ace44e9831e0cf7da563733aa67
joshua <joshuanwosu078@gmail.com>
2025-09-20 20:05:58 +0100

    update

--- components/sections/say-hello.tsx
+++ components/sections/say-hello.tsx
@@ -2,7 +2,7 @@ const SayHello = () => {
   return (
     <section className='py-24 relative'>
       <div className='bg-black w-full h-[500px] absolute bottom-0 left-0 z-10' />
-      <div className='relative grid grid-cols-2 max-w-7xl mx-auto px-4 z-50'>
+      <div className='relative grid grid-cols-2 max-w-7xl mx-auto px-4 z-20'>
         <div>
           <p>Say Hello</p>
         </div>
